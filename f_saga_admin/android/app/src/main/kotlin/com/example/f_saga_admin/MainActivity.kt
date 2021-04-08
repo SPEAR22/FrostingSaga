e1130a1d92b7a68f3761ba3cca8492f7d2455a8e
SPEAR22 <lokeshy142@gmail.com>
2021-04-08 21:36:34 +0530

    INITIALIZATION

---
+++ f_saga_admin/android/app/src/main/kotlin/com/example/f_saga_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f_saga_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ frosting_saga/android/app/src/main/kotlin/com/example/frosting_saga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.frosting_saga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
