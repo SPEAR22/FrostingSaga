41da9c858f922bc49dc24b314a871caeeebd5538
SPEAR22 <lokeshy142@gmail.com>
2021-04-12 15:56:16 +0530

    Day1

---
+++ f_saga_admin/lib/screens/landing_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Container(
+              child: Center(
+                child: Text(snapshot.error),
+              ),
+            ),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            body: Container(
+              child: Center(
+                child: Text("Intialized"),
+              ),
+            ),
+          );
+        }
+
+        return Scaffold(
+          body: Container(
+            child: Center(
+              child: Text("Intializing...."),
+            ),
+          ),
+        );
+        ;
+      },
+    );
+  }
+}
---
+++ frosting_saga/lib/screens/landing_page.dart
@@ -0,0 +1,69 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:frosting_saga/screens/home_page.dart';
+import 'package:frosting_saga/screens/login_page.dart';
+
+class LandingPage extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: Text(snapshot.error)),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, streamSnapshot) {
+                if (streamSnapshot.hasError) {
+                  return Scaffold(
+                    body: Center(
+                      child: Text("Error: ${streamSnapshot.error}"),
+                    ),
+                  );
+                }
+                if (streamSnapshot.connectionState == ConnectionState.active) {
+                  User _user = streamSnapshot.data;
+                  if (_user == null) {
+                    return LoginPage();
+                  } else {
+                    return HomePage();
+                  }
+                }
+                return Scaffold(
+                  body: Center(
+                      child: Stack(
+                    children: [
+                      CircularProgressIndicator(),
+                      Text("Checkin Authentiaction"),
+                    ],
+                  )),
+                );
+              });
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Scaffold(
+          body: Center(
+              child: Stack(
+            children: [
+              CircularProgressIndicator(),
+              Text("Intializing"),
+            ],
+          )),
+        );
+      },
+    );
+  }
+}
